a5126024cffe7e99d646ee939120009894f78354
theprinceofcodes <princejason109@gmail.com>
2026-05-03 09:33:35 +0100

    feat(school): expand school model and validation with additional fields

--- src/modules/school/school.interfaces.ts
+++ src/modules/school/school.interfaces.ts
@@ -8,11 +8,29 @@ export interface ISchool {
   adminUser?: string | null;
   adminUsers?: string[];
   address?: string;
+  schoolCode?: string;
   state?: string;
   localGovernment?: string;
   district?: string;
+  ward?: string;
+  schoolLocation?: string;
+  categoryOfSchool?: string;
+  accessRoadCondition?: string;
+  typeOfSchool?: string;
+  shiftSystem?: string;
+  facilitiesAvailable?: string;
+  headTeacherName?: string;
+  headTeacherPhoneNumber?: string;
+  assistantHeadTeacherName?: string;
+  assistantHeadTeacherPhoneNumber?: string;
   longitude?: number;
   latitude?: number;
+  numberOfClasses?: number;
+  numberOfClassroomsAvailable?: number;
+  numberOfAcademicStaff?: number;
+  numberOfNonAcademicStaff?: number;
+  totalEnrolledStudents?: number;
+  gallery?: string;
   status?: 'active' | 'inactive';
 }
 
