a7fb22e54600ab1dc1904fa0c654f4b7159ce197
theprinceofcodes <princejason109@gmail.com>
2026-05-17 11:31:00 +0100

    Remove seed scripts for subjects and terms, along with related map files and test script for Document AI processing.

--- dist/modules/academic-session/academicSession.interfaces.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=academicSession.interfaces.js.map
--- dist/modules/push-notification/push-notification.interfaces.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=push-notification.interfaces.js.map
